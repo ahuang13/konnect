4d3b3e90009bc317b080df078192e5ad894c2bf1
Angus Huang <angus@edmodo.com>
2014-02-01 22:17:48 -0800

    Initial Commit

---
+++ Konnect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Konnect
+//
+//  Created by Angus Huang on 2/1/14.
+//  Copyright (c) 2014 Angus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Konnect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Konnect
+//
+//  Created by Angus Huang on 2/1/14.
+//  Copyright (c) 2014 Angus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
