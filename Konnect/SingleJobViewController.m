08072b433b17b778419ec9d2e65fa944b573f6df
Fei Xie <feix25@gmail.com>
2014-03-05 12:42:02 -0800

    added single job/candidate views

---
+++ Konnect/SingleJobViewController.m
@@ -0,0 +1,38 @@
+//
+//  SingleJobViewController.m
+//  Konnect
+//
+//  Created by fxie on 3/5/14.
+//  Copyright (c) 2014 Angus Huang. All rights reserved.
+//
+
+#import "SingleJobViewController.h"
+
+@interface SingleJobViewController ()
+
+@end
+
+@implementation SingleJobViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
